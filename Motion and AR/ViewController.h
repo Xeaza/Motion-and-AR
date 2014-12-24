d7ff32bffd6486e1c36b6efaff2ebee7eed56cd8
xeaza <taylorwrightsanson@gmail.com>
2014-12-24 10:36:32 -0500

    Initial Commit

---
+++ Motion and AR/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Motion and AR
+//
+//  Created by Taylor Wright-Sanson on 12/24/14.
+//  Copyright (c) 2014 Taylor Wright-Sanson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
